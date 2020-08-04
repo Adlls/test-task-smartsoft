86b68ac3db55eb9551f85da3be6bb536425325dd
Adlls <adlansaidullaev@gmail.com>
2020-08-04 14:07:32 +0300

    first commit

---
+++ src/main/java/dev/adls/testtasksmartsoft/TestTaskSmartsoftApplication.java
@@ -0,0 +1,13 @@
+package dev.adls.testtasksmartsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSmartsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskSmartsoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/adls/testtasksmartsoft/TestTaskSmartsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adls.testtasksmartsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskSmartsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
